452326248440d045ddf59b92cc501a50312737f9
Jesse Antoszyk <jcantosz@users.noreply.github.com>
2017-02-24 15:24:20 -0500

    Create make_venv.sh

---
+++ setup_tools/make_venv.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+CURR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$(dirname "$CURR")"
+
+cd virtualenv-1.5.2/
+$DIR/.localpython/bin/python setup.py install
+
+echo "Make venv"
+virtualenv -p ${DIR}/.localpython/bin/python  $DIR/venv
+
+source $DIR/venv/bin/activate
+
+python --version
+
+pip install shade
