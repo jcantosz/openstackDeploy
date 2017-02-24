0702da037033562e40928687caa74630f3f647c3
Jesse Antoszyk <jcantosz@users.noreply.github.com>
2017-02-24 15:02:19 -0500

    Update install_ansible.sh

--- setup_tools/install_ansible.sh
+++ setup_tools/install_ansible.sh
@@ -4,8 +4,13 @@ echo "apt-get -qq update"
 sudo apt-get -qq update
 echo "apt-get -qq -y install python-dev libssl-dev libffi-dev"
 sudo apt-get -qq -y install python-dev libssl-dev libffi-dev
+
 echo "pip install pycrypto pyyaml ansible --quiet"
-sudo pip install pycrypto pyyaml ansible shade --quiet
+sudo pip install pycrypto pyyaml ansible pytz --quiet
+
+echo "pip install monotonic funcsigs oslo.config pyparsing futures positional shade --quiet"
+sudo pip install monotonic funcsigs oslo.config pyparsing futures positional shade --quiet
+
 
 echo "apt-get -qq clean"
 sudo apt-get -qq clean
