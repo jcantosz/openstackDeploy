367823d7f65d337969b0d6fe4813afe705d5ca86
Jesse Antoszyk <jcantosz@users.noreply.github.com>
2017-02-27 11:19:51 -0500

    Update install_dependencies.sh

--- setup_tools/install_dependencies.sh
+++ setup_tools/install_dependencies.sh
@@ -3,11 +3,6 @@
 echo "apt-get -qq update"
 sudo apt-get -qq update
 
-echo python --version
-python --version
-echo pip --version
-pip --version
-
 echo "sudo apt-get -qq -y install python-dev libssl-dev libffi-dev"
 sudo apt-get -qq -y install python-dev libssl-dev libffi-dev
 
@@ -15,4 +10,4 @@ echo "sudo apt-get update"
 sudo apt-get -qq update
 
 echo "sudo pip install virtualenv"
-sudo pip install virtualenv
+sudo pip install --quiet virtualenv
